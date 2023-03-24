b634f79f9ea85201169c4511c412b805a03d4e0f
khadidja2023M <6mekik81@solent.ac.uk>
2023-03-24 10:13:06 +0000

    appdate superhost function

--- tui.py
+++ tui.py
@@ -6,18 +6,18 @@
 
 #tui module
 
-#create a starting function that will indicate the start of the programme
+#Create a starting function that will indicate the start of the programme.
 def started(msg=""):  
     dashes=('#')*75
     print(dashes)
     print(f"Operation started: {msg}...\n")
     
-#create a completed function that will indicate the end of the programme    
+#Create a completed function that will indicate the end of the programme.    
 def completed():
     print("Operation completed.")
     dashes=('#')*75
     print(dashes)
-    
+#Create a main menu to give the user a chance to choose what he wants to do.    
 def main_menu():
     print(f"""select which menu would you like:
     {"[first]":<6}:Retrieve data.\n   
@@ -32,18 +32,19 @@ def main_menu():
 def error():  
         print("Invalid Selection!")
 
-#this function display the results of the function listing by id in the process module as a dictionnary.       
+#this function display the results of the function listing by id in the retrieve module as a dictionnary.       
 def display_listing_by_id(ID):
     print(f"host name:{ID['host_name']}\ndescription:{ID['description']}\nhost location:{ID['location']}\nhost   since:{ID['host_since']}")
     
-#this function display the results of the function listing for specified location in the process module as a dictionnary.       
+#this function display the results of the function listing for specified location in the retrieve module as a dictionnary.       
 def display_listing_for_specified_location(location):
     print(f"host name:{location[0]}\nproperty type:{location[1]}\nprice:{location[2]}\nminimum nights:{location[3]}\nmaximum nights:{location[4]}") 
     
-#this function display the results of the function listing by property type in the process module as a dictionnary.   
+#this function display the results of the function listing by property type in the retrieve module as a dictionnary.   
 def display_listing_by_property_type(property_type):
-    print(f"room type:{property_type['room_type']}\naccomodates:{property_type['accomodates']}\nbathrooms:{property_type['bathrooms']}\nbedroom:{property_type['bedroom']}\nbeds:{property_type['beds']}")                             
-#this function display the results of the function listing by location in the process module as a dictionnary.                
+    print(f"room type:{property_type[0]}\naccomodates:{property_type[1]}\nbathrooms:{property_type[2]}\nbedroom:{property_type[3]}\nbeds:{property_type[4]}") 
+    
+#this function display the results of the function listing by location in the retrieve module as a dictionnary.            
 def display_listing_by_location(Location):
     print(f"review scores cleanliness:{Location[0]}\nreview scores communication:{Location[1]}\nreview scores checkin:{Location[2]}\nreview scores rating:{Location[3]}") 
                     
